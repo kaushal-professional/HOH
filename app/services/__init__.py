d7f3fc71b21d0eb6f55beaf2e42d1acba164366d
kaushal-professional <kaushalpatil6794@gmail.com>
2025-12-29 11:05:33 +0530

    Delete app directory

--- app/services/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Service layer for business logic.
-"""
-
-# TODO: Uncomment when these modules are implemented
-# from app.services import purchase, purchase_approval, item_catalog
-# __all__ = ["purchase", "purchase_approval", "item_catalog"]
-
-__all__ = []
